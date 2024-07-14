7f6e52c08d95e7c5a8efb88e860a32aea97ee649
Pontus Sjostedt <po7853sj-s@student.lu.se>
2024-07-14 15:58:36 +0200

    first commit

---
+++ code.lua
@@ -0,0 +1,42 @@
+function DownUntilStop()
+    while turtle.down() do
+    end
+end
+
+function DropAll()
+    for i = 1, 16 do
+        turtle.select(i)
+        turtle.drop()
+    end
+end
+
+function MineSquare(x, z, bias)
+    for _iz = 1, z do
+        for _ix = 1, x - 1 do
+            turtle.dig()
+            turtle.forward()
+        end
+        if _iz ~= z then
+            if (_iz + bias) % 2 == 0 then
+                turtle.turnLeft()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnLeft()
+            else
+                turtle.turnRight()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnRight()
+            end
+        end
+        print(_iz)
+    end
+end
+
+DownUntilStop()
+MineSquare(3, 3, 0)
+turtle.turnLeft()
+turtle.turnLeft()
+turtle.digDown()
+turtle.down()
+MineSquare(3, 3, 0)
